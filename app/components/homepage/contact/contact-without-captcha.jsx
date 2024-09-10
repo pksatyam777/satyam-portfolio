b1071206b822dd8e940b772b4ddc689d2ebe56a3
Satyam <satyam@Satyams-MacBook-Air.local>
2024-09-10 23:37:01 +0530

    added

--- app/components/homepage/contact/contact-without-captcha.jsx
+++ app/components/homepage/contact/contact-without-captcha.jsx
@@ -1,17 +1,17 @@
 "use client";
 // @flow strict
-import { isValidEmail } from '@/utils/check-email';
-import axios from 'axios';
-import { useState } from 'react';
+import { isValidEmail } from "@/utils/check-email";
+import axios from "axios";
+import { useState } from "react";
 import { TbMailForward } from "react-icons/tb";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
 function ContactWithoutCaptcha() {
   const [error, setError] = useState({ email: false, required: false });
   const [userInput, setUserInput] = useState({
-    name: '',
-    email: '',
-    message: '',
+    name: "",
+    email: "",
+    message: "",
   });
 
   const checkRequired = () => {
@@ -29,7 +29,7 @@ function ContactWithoutCaptcha() {
       return;
     } else {
       setError({ ...error, required: false });
-    };
+    }
 
     const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
     const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
@@ -37,19 +37,22 @@ function ContactWithoutCaptcha() {
 
     try {
       const res = await emailjs.send(serviceID, templateID, userInput, options);
-      const teleRes = await axios.post(`${process.env.NEXT_PUBLIC_APP_URL}/api/contact`, userInput);
-
+      const teleRes = await axios.post(
+        `${process.env.NEXT_PUBLIC_APP_URL}/api/contact`,
+        userInput
+      );
+      console.log(res, "hello");
       if (res.status === 200 || teleRes.status === 200) {
-        toast.success('Message sent successfully!');
+        toast.success("Message sent successfully!");
         setUserInput({
-          name: '',
-          email: '',
-          message: '',
+          name: "",
+          email: "",
+          message: "",
         });
-      };
+      }
     } catch (error) {
       toast.error(error?.text || error);
-    };
+    }
   };
 
   return (
@@ -59,7 +62,9 @@ function ContactWithoutCaptcha() {
       </p>
       <div className="max-w-3xl text-white rounded-lg border border-[#464c6a] p-3 lg:p-5">
         <p className="text-sm text-[#d3d8e8]">
-          {"If you have any questions or concerns, please don't hesitate to contact me. I am open to any work opportunities that align with my skills and interests."}
+          {
+            "If you have any questions or concerns, please don't hesitate to contact me. I am open to any work opportunities that align with my skills and interests."
+          }
         </p>
         <div className="mt-6 flex flex-col gap-4">
           <div className="flex flex-col gap-2">
@@ -69,7 +74,9 @@ function ContactWithoutCaptcha() {
               type="text"
               maxLength="100"
               required={true}
-              onChange={(e) => setUserInput({ ...userInput, name: e.target.value })}
+              onChange={(e) =>
+                setUserInput({ ...userInput, name: e.target.value })
+              }
               onBlur={checkRequired}
               value={userInput.name}
             />
@@ -83,15 +90,19 @@ function ContactWithoutCaptcha() {
               maxLength="100"
               required={true}
               value={userInput.email}
-              onChange={(e) => setUserInput({ ...userInput, email: e.target.value })}
+              onChange={(e) =>
+                setUserInput({ ...userInput, email: e.target.value })
+              }
               onBlur={() => {
                 checkRequired();
                 setError({ ...error, email: !isValidEmail(userInput.email) });
               }}
             />
-            {error.email &&
-              <p className="text-sm text-red-400">Please provide a valid email!</p>
-            }
+            {error.email && (
+              <p className="text-sm text-red-400">
+                Please provide a valid email!
+              </p>
+            )}
           </div>
 
           <div className="flex flex-col gap-2">
@@ -101,18 +112,20 @@ function ContactWithoutCaptcha() {
               maxLength="500"
               name="message"
               required={true}
-              onChange={(e) => setUserInput({ ...userInput, message: e.target.value })}
+              onChange={(e) =>
+                setUserInput({ ...userInput, message: e.target.value })
+              }
               onBlur={checkRequired}
               rows="4"
               value={userInput.message}
             />
           </div>
           <div className="flex flex-col items-center gap-2">
-            {error.required &&
+            {error.required && (
               <p className="text-sm text-red-400">
                 Email and Message are required!
               </p>
-            }
+            )}
             <button
               className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-5 md:px-12 py-2.5 md:py-3 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
               role="button"
@@ -126,6 +139,6 @@ function ContactWithoutCaptcha() {
       </div>
     </div>
   );
-};
+}
 
 export default ContactWithoutCaptcha;
--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -45,27 +45,6 @@ function HeroSection() {
             >
               <BsLinkedin size={30} />
             </Link>
-            <Link
-              href={personalData.facebook}
-              target="_blank"
-              className="transition-all text-pink-500 hover:scale-125 duration-300"
-            >
-              <FaFacebook size={30} />
-            </Link>
-            <Link
-              href={personalData.leetcode}
-              target="_blank"
-              className="transition-all text-pink-500 hover:scale-125 duration-300"
-            >
-              <SiLeetcode size={30} />
-            </Link>
-            <Link
-              href={personalData.twitter}
-              target="_blank"
-              className="transition-all text-pink-500 hover:scale-125 duration-300"
-            >
-              <FaTwitterSquare size={30} />
-            </Link>
           </div>
 
           <div className="flex items-center gap-3">
