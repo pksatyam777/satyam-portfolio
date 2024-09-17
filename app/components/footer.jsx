2fff239f2a9a76d881caacae0ae4e068d2962194
Satyam <satyam@Satyams-MacBook-Air.local>
2024-09-17 18:32:23 +0530

    added footer changes

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -24,7 +24,7 @@ function Footer() {
           <div className="flex items-center gap-5">
             <Link
               target="_blank"
-              href="https://github.com/said7388/developer-portfolio"
+              href="https://github.com/pksatyam777/satyam-portfolio"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <IoStar />
@@ -32,7 +32,7 @@ function Footer() {
             </Link>
             <Link
               target="_blank"
-              href="https://github.com/said7388/developer-portfolio/fork"
+              href="https://github.com/pksatyam777/satyam-portfolio/fork"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <CgGitFork />
