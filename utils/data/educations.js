1507d7182c36d49f332824a98c3a35bcdd1b0e7c
pksatyam777 <pksatyam777@gmail.com>
2024-11-04 20:14:09 +0530

    changed education details

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,20 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    title: "Bachelor of Engineering (E & TC)",
+    duration: "2016 - 2020",
+    institution: "PVPIT",
   },
   {
     id: 2,
     title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    duration: "2015 - 2016",
+    institution: "Bharati vidyapeeth (cbse)",
   },
   {
     id: 3,
     title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
-  }
-]
+    duration: "2013 - 2014",
+    institution: "Kendriya vidyalaya (N.D.A , pune)",
+  },
+];
